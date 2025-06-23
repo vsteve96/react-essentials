d332d0751f6fbaba7728c58c18833d13ada900da
Istvan Vizhanyo <vizhanyo.istvan0@gmail.com>
2025-06-23 16:30:31 +0000

    Add StatefulGreeting component with initial state and props handling

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,11 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
-import FunctionalGreeting from './components/FunctionalGreeting';
+import StatefulGreeting from './components/StatefulGreeting';
 
 function App() {
   return (
     <div className="App">
-      <FunctionalGreeting />
+      <StatefulGreeting greeting="I'm a stateful class component!"/>
     </div>
   );
 }
